a24a3861017b9262c85fb1458de7163710c4c224
brian-chu <chuchengpeng@liqunfeideMac-mini.local>
2015-08-19 11:12:11 +0800

    Initial Commit

---
+++ MyNavigationAdding/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNavigationAdding
+//
+//  Created by liqunfei on 15/8/19.
+//  Copyright (c) 2015年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyNavigationAdding/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyNavigationAdding
+//
+//  Created by liqunfei on 15/8/19.
+//  Copyright (c) 2015年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
